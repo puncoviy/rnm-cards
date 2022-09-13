84550a5e4ee9e7a73c1ba12ee54e985ed36cbf74
puncoviy <usaspinner@gmail.com>
2022-09-13 12:51:16 +0300

    feat: add first api page

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ const profileMenu = document.querySelector('.header__profile-link');
 
 const navLink = document.querySelectorAll('.section__navigation-link');
 
+// const cards = document.querySelectorAll('.section__cards-item');
+const cardsList = document.querySelector('.section__cards-list');
+
 burgerBtn.addEventListener('click', function(){
     profileIcon.classList.remove('active');
     profileMenu.classList.remove('active');
@@ -45,31 +48,78 @@ navLink.forEach (el => el.addEventListener('click', function(event){
     el.classList.add('active');
 }))
 
+fetch('https://rickandmortyapi.com/api/character'
+    ).then((res) => res.json()
+    ).then((characters) => getCharacters(characters));
 
 
+function getCharacters({info, results}) {
+    console.log(info);
+    console.log(results);
+    results.forEach(function(el){
+        let newCard = document.createElement('li');
+        newCard.classList.add('section__cards-item');
+        newCard.setAttribute('area-expanded', 'false');
 
-// fetch('https://rickandmortyapi.com/api/character'
-//     ).then((res) => res.json()
-//     ).then(data => getData(data));
+        let newCardIcon = document.createElement('span');
+        newCardIcon.classList.add('icon');
 
+        let newCardImg =  document.createElement('img');
+        newCardImg.classList.add('section__cards-item--img');
+        newCardImg.alt = 'avatar';
+        newCardImg.src = el.image;
 
-// function getData({info, results}) {
+        let newCardTextBox = document.createElement('div');
+        newCardTextBox.classList.add('section__cards-item--box');
+            let newCardTitle = document.createElement('p');
+            newCardTitle.classList.add('section__cards-item--title');
+            newCardTitle.textContent = el.name;
+            newCardTextBox.appendChild(newCardTitle);
 
-// }
+            let newCardSubitle = document.createElement('p');
+            newCardSubitle.classList.add('section__cards-item--subtitle');
+            newCardSubitle.textContent = el.status + ' - ' + el.species;
+            if (el.status === 'Alive') {
+                newCardSubitle.classList.add('alive')
+            } else if (el.status === 'Dead') {
+                newCardSubitle.classList.add('dead')
+            } else {
+                newCardSubitle.classList.add('unknown')
+            }
+            newCardTextBox.appendChild(newCardSubitle);
+        
+        // newCard.textContent = el.id;
+        cardsList.appendChild(newCard);
+        newCard.appendChild(newCardIcon);
+        newCard.appendChild(newCardImg);
+        newCard.appendChild(newCardTextBox);
+    })
 
+    const cards = document.querySelectorAll('.section__cards-item');
 
-const items = document.querySelectorAll(".section__cards-item");
-
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('area-expanded');
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('area-expanded', 'false');
-  }
-  
-  if (itemToggle == 'false') {
-    this.setAttribute('area-expanded', 'true');
-  }
+    function toggleAccordion() {
+        const cardToggle = this.getAttribute('area-expanded');
+        for (i = 0; i < cards.length; i++) {
+        cards[i].setAttribute('area-expanded', 'false');
+        }
+        if (cardToggle == 'false') {
+        this.setAttribute('area-expanded', 'true');
+        }
+    }
+    
+    cards.forEach(el => el.addEventListener('click', toggleAccordion));
+    console.log(cards)
 }
 
-items.forEach(item => item.addEventListener('click', toggleAccordion));
+// function toggleAccordion() {
+//     const cardToggle = this.getAttribute('area-expanded');
+//     for (i = 0; i < cards.length; i++) {
+//       cards[i].setAttribute('area-expanded', 'false');
+//     }
+//     if (cardToggle == 'false') {
+//       this.setAttribute('area-expanded', 'true');
+//     }
+//   }
+  
+// cards.forEach(el => el.addEventListener('click', toggleAccordion));
+// console.log(cards)
